ba3d983deeefec2f032807e5c14d1dc3a55a1bb7
klongrich <longrichk@gmail.com>
2022-06-07 14:20:26 -0400

    seprate branch example

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// This comment will be the only difference when I merge the PR
+
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
