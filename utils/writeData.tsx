410bf1202d06d762efe09ee940d4bf9291f13cd3
klongrich <longrichk@gmail.com>
2022-06-13 03:41:13 -0400

    updating styling

--- utils/writeData.tsx
+++ utils/writeData.tsx
@@ -2,7 +2,7 @@ import { getDatabase, ref, set} from "firebase/database";
 
 export async function writeData(userAddress : string, toAddress : string, message : string) {
     const database = getDatabase();
-    const currentTime = await new Date().getTime();
+    const currentTime = await new Date().getTime()
 
     set(ref(database, 'messages/' + toAddress.toLowerCase() + '/unread/'+ userAddress.toLowerCase() + '/' + currentTime.toString()), {
       From: userAddress,
