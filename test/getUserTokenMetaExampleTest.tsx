e23081a3a3a11b1b231ba55a28cc429c1cfe083a
klongrich <longrichk@gmail.com>
2022-06-14 00:08:17 -0400

    pushing first test

--- test/getUserTokenMetaExampleTest.tsx
+++ test/getUserTokenMetaExampleTest.tsx
@@ -2,9 +2,9 @@
 import { GetUserTokenMeta } from "../utils/eth/getUserTokenMeta";
 
 export async function ExampleTest() {
-    let metaData = await GetUserTokenMeta("0x5cbd390539caaa24152d62003de2522565470152")
+    // let metaData = await GetUserTokenMeta("0x5cbd390539caaa24152d62003de2522565470152")
 
-    console.log("----Printing User MetaData----")
-    console.log(metaData);
+    // console.log("----Printing User MetaData----")
+    // console.log(metaData);
 }
 
