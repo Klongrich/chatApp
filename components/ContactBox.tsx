7e0a5e50ce09795cdcef2bc2df34a2b3346e2fbc
klongrich <longrichk@gmail.com>
2022-06-18 22:23:25 -0400

    updating sync function in contact box

--- components/ContactBox.tsx
+++ components/ContactBox.tsx
@@ -9,81 +9,80 @@ import { Container,
          SyncedBox } from "../styles/ContactBox";
 
 import  ContactMeta  from "../components/ContactMeta";
+interface ContactInfo {
+    alias : string;
+    from : string;
+    isSynced : boolean;
+    isSyncing : boolean;
+}
 
 export const ContactBox = ({userAddress, updateToChatRoom, contactList} : any) => {
 
-    const [displayContactMeta, setDisplayContactMeta] = useState(false);
-    const [allContactsInfo, setAllContactsInfo] = useState([{}]);
-
-    const [isSyncing, setIsSyncing] = useState(false);
-    const [isSynced, setIsSynced] = useState(false);
-    const [confrimingSync, setConfrimingSync] = useState(false);
+    const [displayContactMeta, setDisplayContactMeta] = useState<boolean>(false);
+    const [allContactsInfo, setAllContactsInfo] = useState<ContactInfo [] | null>([]) ;
 
     const [contactAddress, setContactAddress] = useState("");
     const [contactAlias, setContactAlias] = useState("");
 
-    //FAKE storage call
-    async function allStorage() {
-        var valuesX = [],
-            keys = await Object.keys(localStorage),
-            i = keys.length;
-
-        while ( i-- ) {
-            valuesX.push(localStorage.getItem(keys[i]) );
-        }
-        return valuesX;
-    }
-
-    async function loadUserInfo() {
-        let userCache = await allStorage();
-
-        let cachedContent = [{
-            alias : "",
-            publicKey: ""
-        }];
-
-        console.log(userCache);
-
-        for (let x = 0; x < userCache.length; x++) {
-            //@ts-ignore
-            if (userCache[x].includes("0x")) {
-                //@ts-ignore
-
-                console.log(userCache[x]);
-
-                //@ts-ignore
-                let _temp = userCache[x].split(":");
-
-                if (_temp[1]) {
-                    let userPublicKey = _temp[0].trim();
-
-                    if (userPublicKey.length == 42) {
-                        let _userContacts = {
-                            alias : _temp[1].trim(),
-                            publicKey : _temp[0].trim()
-                        }
-                        cachedContent.push(_userContacts);
-                    }
-                }
-            }
-        }
-        setAllContactsInfo(cachedContent);
-    }
-
     async function getSyncStatus(from : string) {
-        //pull from local storage;
         let syncKey = from + userAddress + "sync";
         let status = await localStorage.getItem(syncKey);
 
         if (status) {
-            return ("true")
+            return (true)
         } else {
-            return ("false");
+            return (false);
         }
     }
 
+    async function syncContact(contractAddress : string) {
+        let res : any = []
+
+        if (allContactsInfo != null) {
+            for (let x = 0; x < allContactsInfo.length; x++) {
+                if (allContactsInfo[x].from == contractAddress) {
+                    let updateSync = {
+                        alias : allContactsInfo[x].alias,
+                        from : allContactsInfo[x]. from,
+                        isSynced : false,
+                        isSyncing : true
+                    }
+                    res.push(updateSync);
+                } else {
+                    res.push(allContactsInfo[x]);
+                }
+            }
+            setAllContactsInfo(res);
+        }
+
+        await delay(2000);
+
+        let syncKey = contractAddress + userAddress + "sync";
+        await localStorage.setItem(syncKey, "true");
+
+        let res2 : any = [];
+
+        if (allContactsInfo != null) {
+            for (let x = 0; x < allContactsInfo.length; x++) {
+                if (allContactsInfo[x].from == contractAddress) {
+                    let updateSync = {
+                        alias : allContactsInfo[x].alias,
+                        from : allContactsInfo[x]. from,
+                        isSynced : true,
+                        isSyncing : true
+                    }
+                    res2.push(updateSync);
+                } else {
+                    res2.push(allContactsInfo[x]);
+                }
+            }
+            setAllContactsInfo(res2);
+        }
+
+    }
+
     async function checkSyncStatus(contactList : any) {
-        let _res : any = [];
+        let _res : ContactInfo[] = [];
 
         for (let x = 0; x < contactList.length; x++) {
 
@@ -92,21 +91,16 @@ export const ContactBox = ({userAddress, updateToChatRoom, contactList} : any) =
 
             let _sync = await getSyncStatus(_from);
 
-            console.log("---Sync Status---");
-            console.log(_sync);
-
             if (_from && _alias) {
-
                 let _dataObject = {
                     from : contactList[x].from,
                     alias : contactList[x].alias,
-                    sync : _sync
+                    isSynced : _sync,
+                    isSyncing : false
                 }
                 _res.push(_dataObject);
             }
         }
-
-        console.log(_res);
         setAllContactsInfo(_res);
     }
 
@@ -126,19 +120,6 @@ export const ContactBox = ({userAddress, updateToChatRoom, contactList} : any) =
 
     function delay(ms: number) {
         return new Promise( resolve => setTimeout(resolve, ms) );
-    }
-
-    async function syncContact(contactAddress : string) {
-        console.log(contactAddress);
-        setIsSyncing(true);
-        await delay(2000);
-
-        //write to cache
-        let syncKey = contactAddress + userAddress + "sync";
-        await localStorage.setItem(syncKey, "true");
-
-        setIsSyncing(false);
-        setIsSynced(true);
     }
 
     async function showContactMeta(contactAddress : string, contactAlias : string) {
@@ -164,59 +145,39 @@ export const ContactBox = ({userAddress, updateToChatRoom, contactList} : any) =
 
             {!displayContactMeta && <>
                 <Container>
-                {allContactsInfo.map((data : any) =>
-                    <>
-                        {data.from != "" && data.alias != "true,\"accounts\"" && data.alias != "" && <>
-                        <ContactContaier onClick={() => showContactMeta(data.from, data.alias)}>
-                            <ProfilePicBox />
-                            <h4> <strong> {data.alias} </strong> </h4>
-                            <ContactsPublicKeyBox>
-                                <p> {cutUserAddress(data.from)} </p>
-                            </ContactsPublicKeyBox>
-                        </ContactContaier>
-                        <SyncBox>
-                            <h5> Sync </h5>
-                        </SyncBox>
-                        <br /> <br />
+                    {allContactsInfo != null && <>
+                        {allContactsInfo.map((data : any) => <>
+                            {data.from != "" && data.alias != "true,\"accounts\"" && data.alias != "" && <>
+                                <ContactContaier onClick={() => showContactMeta(data.from, data.alias)}>
+                                    <ProfilePicBox />
+                                    <h4> <strong> {data.alias} </strong> </h4>
+                                    <ContactsPublicKeyBox>
+                                        <p> {cutUserAddress(data.from)} </p>
+                                    </ContactsPublicKeyBox>
+                                </ContactContaier>
 
-                        {/* {!isSynced && !isSyncing && <>
-                            {confrimingSync && <>
-                                <SyncBox onClick={() => syncContact(data.from)}>
-                                    Confrim Sync
-                                </SyncBox>
-
-                            </>}
-                            {!confrimingSync && <>
-                                {data.sync == "false" && <>
-                                <SyncBox onClick={() => setConfrimingSync(true)}>
-                                    <h5> Sync </h5>
-                                </SyncBox>
+                                {data.isSynced != true && data.isSyncing != true && <>
+                                    <SyncBox onClick={() => syncContact(data.from)}>
+                                        <h5> Sync </h5>
+                                    </SyncBox>
                                 </>}
-                                {data.sync == "true" && <>
+
+                                {data.isSynced != true && data.isSyncing == true && <>
+                                    <SyncingBox>
+                                        <h5> Syncing .... </h5>
+                                    </SyncingBox>
+                                </>}
+
+                                {data.isSynced && <>
                                     <SyncedBox>
                                         <h5> Synced </h5>
                                     </SyncedBox>
                                 </>}
+
+                                <br /> <br />
                             </>}
-                        </>}
-
-                        {!isSynced && isSyncing && <>
-                            <SyncingBox>
-                                <h5> Syncing ..... </h5>
-                            </SyncingBox>
-                        </>}
-
-                        {isSynced && <>
-                            <SyncedBox>
-                                <h5> Synced </h5>
-                            </SyncedBox>
-                        </>}
-
-                        <br /> <br /> */}
-
-                        </>}
-                    </>
-                )}
+                        </>)}
+                    </>}
                 </Container>
             </>}
         </>
